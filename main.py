8e48b6bc230efd4e9ed0f28513b4ffa1737e20e8
kennysc <kenny.stcyr@outlook.com>
2025-09-17 20:41:31 -0400

    added arguments

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from stats import *
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1) 
+
 def main():
-    path = "books/frankenstein.txt"
+    path = sys.argv[1]
     book = get_book_text(path)
     nbr_words = word_count(book)
     characters = reorder_dict(book)
